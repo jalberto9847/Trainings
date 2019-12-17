f669c9186f2ce57dd7752912d104d4e4e7809b31
jalberto9847 <josealberto.ch.b@gmail.com>
2019-12-17 10:17:27 -0600

    Added component properties

--- React/Training_1/example1/react-is-fun/src/index.js
+++ React/Training_1/example1/react-is-fun/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom'
 
 class Message extends React.Component {
   render() {
+    console.log(this.props)
     return(
       <div>
-        <h1>Hello Everyone</h1>
+        <h1 style={{color: this.props.color}}>
+        {this.props.msg}
+        </h1>
+        <p>I will check back in {this.props.minutes} minutes</p>
       </div>
     )
   }
 }
 
-ReactDOM.render(<Message />, document.getElementById('root'))
+ReactDOM.render(
+  <Message color="blue" msg="How are you?" minutes={5} />,
+  document.getElementById('root')
+)
