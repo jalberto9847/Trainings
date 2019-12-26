c632ff31621ae99a3ea1619c03f5b2560691f559
jalberto9847 <josealberto.ch.b@gmail.com>
2019-12-26 11:44:29 -0600

    Modularizing code

---
+++ React/Training_1/react-is-fun/src/Book.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Book = ({title="No Title Provided", author="No Author", pages="0", freeBookmark}) => {
+  return(
+    <section>
+      <h2>{title}</h2>
+      <p>by: {author}</p>
+      <p>Pages: {pages} page</p>
+      <p>Free Boormark today: {freeBookmark ? 'yes' : 'no'}</p>
+    </section>
+  )
+}
+
