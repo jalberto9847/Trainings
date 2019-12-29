3a57be1c53d2bff9416c0bf0169004f996da49d6
jalberto9847 <josealberto.ch.b@gmail.com>
2019-12-28 18:53:38 -0600

    Render a template

--- Ruby_on_rails/Training_1/simple_cms/config/routes.rb
+++ Ruby_on_rails/Training_1/simple_cms/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'demo#index'
 
   get 'demo/index'
+  get 'demo/hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
