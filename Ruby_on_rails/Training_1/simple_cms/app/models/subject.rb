f0cda962b07e13c1a61ede9a83c80f11573378e2
jalberto9847 <josealberto.ch.b@gmail.com>
2020-01-19 19:21:04 -0600

    Named scopes

--- Ruby_on_rails/Training_1/simple_cms/app/models/subject.rb
+++ Ruby_on_rails/Training_1/simple_cms/app/models/subject.rb
@@ -1,2 +1,11 @@
 class Subject < ApplicationRecord
+<<<<<<< HEAD
+
+  scope :visible, lambda { where(:visible => true) }
+  scope :invisible, lambda { where(:visible => false) }
+  scope :sorted, lambda { order("position ASC") }
+  scope :newest_first, lambda { order("created_at DESC") }
+  scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"]) }
+=======
+>>>>>>> parent of 8bece113... Named scopes
 end
